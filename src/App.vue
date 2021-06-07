6234c53dfe9f7b7848a6d3782ea7e68c95672a81
Pham Hieu <honglangtoacan1@gmail.com>
2021-06-07 22:00:29 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Combobox />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Combobox from "./components/Combobox.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Combobox,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Combobox.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="combobox-container" @focusout="closeOptions">
+    <div class="combobox-field" @click="isShow = !isShow"></div>
+    <div class="combobox-options" v-if="isShow">
+      <div class="option"></div>
+      <div class="option"></div>
+      <div class="option"></div>
+      <div class="option"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: false,
+    };
+  },
+  methods: {
+    closeOptions() {
+      this.isShow = false;
+      console.log("focusout");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.combobox-container {
+  width: 300px;
+  position: relative;
+}
+.combobox-field {
+  width: 300px;
+  height: 42px;
+  border: 1px solid #000;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.combobox-options {
+  position: absolute;
+  width: 100%;
+  top: calc(100% + 3px);
+  border: 1px solid #000;
+  border-radius: 5px;
+}
+.option {
+  cursor: pointer;
+  height: 42px;
+}
+.option:hover {
+  background: greenyellow;
+}
+</style>
